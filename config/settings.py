3cee4caada11422b88045a417eb7f80ee7eafafc
San <san0558@naver.com>
2020-10-04 19:21:30 +0900

    # 3.1 Introduction to the User Model

--- config/settings.py
+++ config/settings.py
@@ -29,11 +29,14 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+
 DJANGO_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.staticfiles",
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
 ]
 
 PROJECT_APPS = ["users.apps.UsersConfig"]
