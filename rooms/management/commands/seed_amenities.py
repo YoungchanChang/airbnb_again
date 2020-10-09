018eee3e962344f7c262474c7ed56e58a7292e6d
San <san0558@naver.com>
2020-10-09 13:03:39 +0900

    #9.1 seed_amenities

---
+++ rooms/management/commands/seed_amenities.py
@@ -0,0 +1,38 @@
+from django.core.management.base import BaseCommand, CommandError
+from rooms.models import Amenity
+class Command(BaseCommand):
+    print("hello")
+
+    help = "This command tells me that he loves me"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--times", help="How many time do you wnat me to tell you that i love you?"
+        )
+
+    def handle(self, *args, **options):
+        amenities = [
+            "난방",
+            "에어컨",
+            "세탁기",
+            "건조기",
+            "무선 인터넷",
+            "아침식사",
+            "실내 벽난로",
+            "옷걸이",
+            "다리미",
+            "헤어드라이어",
+            "노트북 작업 공간",
+            "TV",
+            "아기 침대",
+            "유아용 식탁의자",
+            "셀프 체크인",
+            "화재경보기",
+            "일산화탄소 경보기",
+            "욕실 단독 사용",
+            "피아노",
+            "해변에 인접",
+        ]
+        for a in amenities:
+            Amenity.objects.create(name=a)
+        self.stdout.write(self.style.SUCCESS("Amenities created!!!"))
