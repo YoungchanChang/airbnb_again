b281d8f5265d5dc6ab633509528b15d9e5d34f23
San <san0558@naver.com>
2020-10-10 12:37:53 +0900

    #12.0 URL(namespace, name) and Arguments

--- rooms/views.py
+++ rooms/views.py
@@ -2,6 +2,7 @@ from math import ceil
 from django.shortcuts import render, redirect
 from django.core.paginator import Paginator, EmptyPage
 from . import models
+from django.shortcuts import render
 from django.views.generic import ListView
 
 class HomeView(ListView):
@@ -12,3 +13,8 @@ class HomeView(ListView):
     paginate_by = 10
     paginate_orphans = 5
     ordering = "created"
+    context_object_name = "rooms"
+
+def room_detail(request, pk):
+    print(pk)
+    return render(request, "rooms/detail.html")
