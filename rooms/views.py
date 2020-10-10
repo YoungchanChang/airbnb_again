0758071b63b1dba5f840d0b4d1dca8f67e62db32
San <san0558@naver.com>
2020-10-10 15:02:08 +0900

    #13.0 search template, form, url

--- rooms/views.py
+++ rooms/views.py
@@ -5,6 +5,7 @@ from . import models
 from django.urls import reverse
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.shortcuts import render
 
 class HomeView(ListView):
 
@@ -22,3 +23,8 @@ class RoomDetail(DetailView):
 
     model = models.Room
 
+
+def search(request):
+    city = request.GET.get("city")
+    city = str.capitalize(city)
+    return render(request, "rooms/search.html", {"city": city})
