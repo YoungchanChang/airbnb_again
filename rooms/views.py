ca85ce321fe73b2a6a2390a22317e221ac621b36
San <san0558@naver.com>
2020-10-10 14:45:59 +0900

    #12.3 DetailView CBV

--- rooms/views.py
+++ rooms/views.py
@@ -4,7 +4,7 @@ from django.core.paginator import Paginator, EmptyPage
 from . import models
 from django.urls import reverse
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 class HomeView(ListView):
 
@@ -16,9 +16,9 @@ class HomeView(ListView):
     ordering = "created"
     context_object_name = "rooms"
 
-def room_detail(request, pk):
-    try:
-        room = models.Room.objects.get(pk=pk)
-        return render(request, "rooms/detail.html", {"room": room})
-    except models.Room.DoesNotExist:
-        return redirect(reverse("core:home"))
+class RoomDetail(DetailView):
+
+    """ RoomDetail Definition """
+
+    model = models.Room
+
