d7a5a4728e3272511575d44ffd18446803b96385
San <san0558@naver.com>
2020-10-10 11:32:35 +0900

    #11.4 Using Django Paginator

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,6 @@
 from math import ceil
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from . import models
 
 def all_rooms(request):
@@ -8,11 +9,18 @@ def all_rooms(request):
     current_page = int(current_page or 1)
 
     # List설정부분
+    # page_size = 5
+    # limit = page_size * current_page
+    # offset = limit - page_size
+    # all_rooms = models.Room.objects.all()[offset:limit]
+
+    #위 코드가 아래로 대체
     page_size = 5
-    limit = page_size * current_page
-    offset = limit - page_size
-    all_rooms = models.Room.objects.all()[offset:limit]
-    
+    room_list = models.Room.objects.all()
+    paginator = Paginator(room_list, page_size)
+    rooms = paginator.get_page(current_page)
+
+
     # Navigator 설정부분
     block_size = 5
     current_block = ceil(current_page / block_size)
@@ -21,7 +29,8 @@ def all_rooms(request):
     block_offset = block_limit - block_size + 1
 
     # 마지막 페이지 설정 부분
-    max_page = ceil(models.Room.objects.count() / page_size)
+    # max_page = ceil(models.Room.objects.count() / page_size)
+    max_page = paginator.num_pages
     block_end = current_block * block_size
     if(max_page <= block_end):
         block_end = max_page
@@ -30,9 +39,10 @@ def all_rooms(request):
         request,
         "rooms/home.html",
         {
-            "potato": all_rooms,
+            "rooms": rooms,
             "page": current_page,
             "max_page": max_page,
+            "block_end" : block_end,
             "page_range": range(block_offset, block_end+1),
         },
     )
