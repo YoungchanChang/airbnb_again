946406f5161a543fc70392d1476237d60d97c10e
San <san0558@naver.com>
2020-10-11 16:38:26 +0900

    #16.1 Verify Email

--- config/settings.py
+++ config/settings.py
@@ -143,4 +143,4 @@ EMAIL_HOST = "smtp.mailgun.org"
 EMAIL_PORT = "587"
 EMAIL_HOST_USER = os.environ.get("MAILGUN_USERNAME")
 EMAIL_HOST_PASSWORD =os.environ.get("MAILGUN_PASSWORD")
-
+EMAIL_FROM = "noreply@sandbox2ba559537f904296851b8b1b0c8d7d24.mailgun.org"
