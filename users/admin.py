4fae4c0ad22d1cb4ba3e165ea3380707340a97d9
San <san0558@naver.com>
2020-10-11 21:32:36 +0900

    #17.0 get access token / user에게 githublogin요청 / redirect 처리

--- users/admin.py
+++ users/admin.py
@@ -26,6 +26,7 @@ class CustomUserAdmin(UserAdmin):
                     "language",
                     "currency",
                     "superhost",
+                    "login_method",
                 )
             },
         ),
@@ -45,4 +46,5 @@ class CustomUserAdmin(UserAdmin):
         "is_superuser",
         "email_verified",
         "email_secret",
+        "login_method",
     )
